0b02ec875a67300659798fd7cfe81d9aa93f90df
Kauan Caruso <k4u4n.br1to@gmail.com>
2023-02-04 15:25:24 -0300

    Modificacao Exercicio Tres

--- javaaula2tb/Exercicio3.java
+++ javaaula2tb/Exercicio3.java
@@ -2,6 +2,9 @@ package ads.javaaula2tb;
 public class Exercicio3 {
     public static void main(String[] args) {
         double resultado, n1, n2;
-        
+        //True
+        //
+        //
+        //True
     }
 }
