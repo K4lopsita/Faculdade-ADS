ae4c5d81e3035bad21b9b1896119c30b6447111e
Kauan Caruso <123602468+K4lopsita@users.noreply.github.com>
2023-02-08 19:08:13 -0300

    Alteração Exercício 1

--- javaaula2tb/Exercicio1.java
+++ javaaula2tb/Exercicio1.java
@@ -9,13 +9,11 @@ public class Exercicio1 {
         Scanner scNum = new Scanner(System.in);
         System.out.println("Digite seu nome:");
         String nome = scAlfa.nextLine();
-        System.out.println("O nome digitado foi: " + nome);
         System.out.println("Digite sua RA");
         int RA = scNum.nextInt();
-        System.out.println("O seu RA é: " + RA);
         System.out.println("Digite seu curso:");
         String curso = scAlfa.nextLine();
-        System.out.println("O seu curso é: " + curso);
+        
+        System.out.println("Nome: " + nome + " RA: " + RA + " Curso: " + curso);
     }
 }
- 
