505f0093680c114fab331ec75d0301772e546dbd
Kauan Caruso <123602468+K4lopsita@users.noreply.github.com>
2023-02-08 19:48:01 -0300

    Alteração Exercício 3

--- javaaula2tb/Exercicio3.java
+++ javaaula2tb/Exercicio3.java
@@ -2,8 +2,8 @@ package ads.javaaula2tb;
 public class Exercicio3 {
     public static void main(String[] args) {
         //True
-        //
-        //
         //True
+        //False
+        //False
     }
 }
