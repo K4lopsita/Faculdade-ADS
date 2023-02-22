c261fd6f9382d930c588091f2f34f0163614337f
Kauan Caruso <123602468+K4lopsita@users.noreply.github.com>
2023-02-22 19:52:53 -0300

    Add files via upload

---
+++ javaaula1tb/MeuPrimeiroPrograma.java
@@ -0,0 +1,7 @@
+package ads.javaaula1tb;
+public class MeuPrimeiroPrograma {
+    //Comentário Curto
+    public static void main(String[] args) {
+        System.out.println("Hello World"); //Imprime a palavra Hello World
+    }
+}
